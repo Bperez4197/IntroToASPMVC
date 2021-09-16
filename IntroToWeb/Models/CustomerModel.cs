498ece3d61a346bd72ebf07d7146d8c12ac0c2d8
Bryce Perez <bryceperez31@gmail.com>
2021-09-15 18:25:03 -0700

    added functionality

--- IntroToWeb/Models/CustomerModel.cs
+++ IntroToWeb/Models/CustomerModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 
 
@@ -9,8 +10,17 @@ namespace IntroToWeb.Models
 {
     public class CustomerModel
     {
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Business Name")]
         public string BusinessName { get; set; }
+        [DataType(DataType.Currency)]
+        [Display(Name = "Project Cost")]
+        public decimal ProjectCost { get; set; }
     }
 }
